e9cb810f5302aebe7c95a06261e02a9d83eafcfd
Otto <otto.andre@deep-north.com>
2018-09-19 22:19:24 +0200

    added content to api.js

---
+++ api.js
@@ -0,0 +1,7 @@
+/*
+Create user:
+METHOD: POST
+ENDPOINT: api/user
+BODY (json): {"name":"Mr.Fluffy","email":"fluffy@uia.no","password":"12345678"}
+RESPONSE (json): {"id":1,"name":"Mr.Fluffy","email":"fluffy@uia.no","password":"12345678"}
+*/
